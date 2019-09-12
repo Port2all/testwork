226354b62601452e3cf9be33f2466e2f7b6292be
AndreyK <andre1cheg75@gmail.com>
2019-09-12 15:44:46 +0300

    Added last modified date beautification

--- src/components/File.vue
+++ src/components/File.vue
@@ -3,19 +3,68 @@
         <td>File</td>
         <td>{{ name }}</td>
         <td>{{ size }}</td>
-        <td>{{ date }}</td>
+        <td>{{ new Date(date).toLocaleString()  }}</td>
     </tr>
 </template>
 
 <script>
+
 
     export default {
         name: 'File',
         props: {
             name: String,
             size: Number,
-            date: String
+            date: String,
+        },
+        /*data() {
+            return {
+                formatedDate: 'изменено' + formatDate(this.date),
+            }
+        },*/
+        methods: {
+
+        },
+        created(){
+
+           /* function formatDate(date) {
+                let time = new Date(Date.parse(date));
+
+                let diff = new Date() - this.date; // разница в миллисекундах
+
+                if (diff < 1000) { // меньше 1 минуты
+                    return 'прямо сейчас';
+                }
+
+                let sec = Math.floor(diff / 1000); // преобразовать разницу в секунды
+
+                if (sec < 60) {
+                    return sec + ' сек. назад';
+                }
+
+                let min = Math.floor(diff / 60000); // преобразовать разницу в минуты
+                if (min < 60) {
+                    return min + ' мин. назад';
+                }
+
+                // отформатировать дату
+                // добавить ведущие нули к единственной цифре дню/месяцу/часам/минутам
+                let d = date;
+                d = [
+                    '0' + d.getDate(),
+                    '0' + (d.getMonth() + 1),
+                    '' + d.getFullYear(),
+                    '0' + d.getHours(),
+                    '0' + d.getMinutes()
+                ].map(component => component.slice(-2)); // взять последние 2 цифры из каждой компоненты
+
+                // соединить компоненты в дату
+                return d.slice(0, 3).join('.') + ' ' + d.slice(3).join(':');
+            }
+*/
+
         }
+
     }
 
 
